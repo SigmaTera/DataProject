880cbc8e948cf0ce8507fbf21c68ba560d1293cc
SigmaTera <394516098@qq.com>
2018-10-09 23:03:28 +0800

    1

--- CurlExLauncher/CurlExLauncher.cpp
+++ CurlExLauncher/CurlExLauncher.cpp
@@ -4,17 +4,41 @@
 #include "stdafx.h"
 
 
-int main()
+int _tmain(int argc, LPTSTR argv[])
 {
 	CString strParam;
+	CString strCurlPath;
+	CString strFilePath;
+
 	int nStep = 100000;
 	int n = 2000000;//4783821;
+	int nProcesses = 10;
+
+	strFilePath = TEXT("I:\\DataFile");
+
+	if (argc > 4)
+	{
+		strFilePath = argv[1];
+		n = _ttoi(argv[2]);
+		nStep = _ttoi(argv[3]);
+		nProcesses = _ttoi(argv[4]);
+	}
+	
+	
+
+	GetModuleFileName(NULL, strCurlPath.GetBuffer(MAX_PATH), MAX_PATH);
+	strCurlPath.ReleaseBuffer();
+	PathRemoveFileSpec(strCurlPath.GetBuffer(MAX_PATH));
+	strCurlPath.ReleaseBuffer();
+
+	strCurlPath += TEXT("\\CurlEx.exe");
+
 	for (int i = 0; i < 10; i++)
 	{
 		
-		strParam.Format(TEXT("D:\\DataFile %d %d +"),n,nStep);
+		strParam.Format(TEXT("%s %d %d +"),strFilePath,n,nStep);
 		n += nStep;
-		ShellExecute(NULL, TEXT("Open"), TEXT("CurlEx.exe"), strParam, NULL, SW_SHOW);
+		ShellExecute(NULL, TEXT("Open"), strCurlPath, strParam, NULL, SW_SHOW);
 	}
 	return 0;
 }
